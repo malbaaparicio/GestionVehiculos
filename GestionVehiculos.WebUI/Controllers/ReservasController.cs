4c5d078c3be969d86ae9a88dd114ffeaf5de20ed
malbaaparicio <miguelalbaap@gmail.com>
2024-02-08 00:13:09 +0100

    Gestión de reservas. Formulario de edición con desplegables

--- GestionVehiculos.WebUI/Controllers/ReservasController.cs
+++ GestionVehiculos.WebUI/Controllers/ReservasController.cs
@@ -12,11 +12,17 @@ namespace GestionVehiculos.WebUI.Controllers
     public class ReservasController : Controller
     {
         public IReservas reservas;
+        public ICoches coches;
+        public IDelegaciones delegaciones;
+        public IClientes clientes;
         public int pageSize = 8;
 
-        public ReservasController(IReservas reservas)
+        public ReservasController(IReservas reservas, ICoches coches, IDelegaciones delegaciones, IClientes clientes)
         {
-            this.reservas = reservas;            
+            this.reservas = reservas;      
+            this.coches = coches;
+            this.delegaciones = delegaciones;
+            this.clientes = clientes;
         }
 
 
@@ -54,6 +60,21 @@ namespace GestionVehiculos.WebUI.Controllers
         public ViewResult Edit(int reservaId)
         {
             Reservas reserva = reservas.GetReservas.Where(z => z.Reservarid == reservaId).FirstOrDefault();
+            ViewBag.Coches = coches.GetCoches.Select(z => new SelectListItem
+            {
+                Text = z.Marca + " " +z.Modelo + " " + z.Matricula,
+                Value = z.Cocheid.ToString()
+            });
+            ViewBag.Clientes = clientes.GetClientes.Select(z => new SelectListItem
+            {
+                Text = z.Nombre + " " + z.Apellidos,
+                Value = z.Clienteid.ToString()
+            });
+            ViewBag.Delegaciones = delegaciones.GetDelegaciones.Select(z => new SelectListItem
+            {
+                Text = z.Nombre,
+                Value = z.Delegacionid.ToString()
+            });
             return View(reserva);
         }
 
